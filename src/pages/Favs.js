d649f588922857d5e90b8a4273c54032510fe885
Kylie Clark <kyliedarlene@icloud.com>
2024-01-26 01:49:04 -0700

    copied Cuties to Favs and changed Header props

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,13 @@
+import Header from '../components/Header';
+import Feed from '../components/Feed';
+
 function Favs() {
-    return (
-      <>
-        <h1>Favs!</h1>
-      </>
-    );
-  };
+  return (
+    <div className='page'>
+      <Header to={"/cuties"} text={"See All Cuties"} />
+      <Feed />
+    </div>
+  );
+};
   
   export default Favs;
